c562b270dd70d63604e41db17fb05bbbd51479b5
sanada08 <sanada.yukimura@beldex.io>
2022-06-02 12:00:45 +0530

    first commit

---
+++ beldex-ios-wallet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  beldex-ios-wallet
+//
+//  Created by Mac on 6/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ beldex-ios-walletUITests/beldex_ios_walletUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  beldex_ios_walletUITestsLaunchTests.swift
+//  beldex-ios-walletUITests
+//
+//  Created by Mac on 6/2/22.
+//
+
+import XCTest
+
+class beldex_ios_walletUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
